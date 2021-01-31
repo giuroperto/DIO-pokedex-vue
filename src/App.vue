1f2574802d714ec61d19432c6f24bfa4233edd76
giuroperto <giu.roperto@gmail.com>
2021-01-31 10:31:07 -0200

    starting styles but missing a commit from setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
 	<div id="pokedex">
-    <h1 class="text--yellow">Pokedex DIO</h1>
+
+<!-- // ponto de inicio -> componente pokedex -->
+    <Pokedex />
 	</div>
 </template>
 
 <script>
 
+import Pokedex from './components/Pokedex.vue';
+
 export default {
-	name: "App",
+  name: "App",
+  components: {
+    Pokedex,
+  }
 };
 </script>
 
@@ -15,9 +22,9 @@ export default {
   #pokedex {
     display: flex;
     flex-direction: column;
-    min-height: 100vh;
-    max-width: 1000px;
-    margin: 0 auto;
-    padding: 24px;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 20px);
+    padding: 20px;
   }
 </style>
